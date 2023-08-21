2ee30d17f3d69150c1984254c91bbaf23f228870
hellen atanasio <l.atanasio@hotmail.com>
2023-08-21 14:29:42 -0300

    #

---
+++ src/TiposVariaveis.java
@@ -0,0 +1,10 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+        double salarioMinimo = 2500.33;
+        System.out.print(salarioMinimo);
+        float PI = 3.14F;
+        System.out.println(PI);
+        
+    }
+
+}
